f194cf7267322f03ef01e563297d0728b38db69e
Tom Ho <datomster21@gmail.com>
2016-08-05 17:26:41 -0500

    add decks links and nav logic for user auth

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Deck.create(name: "Ruby")
+Deck.create(name: "Pokemon")
+Deck.create(name: "Cats")
+Deck.create(name: "The Boy Who Lived")
+Deck.create(name: "Tacos")
+Deck.create(name: "The Room")
+
