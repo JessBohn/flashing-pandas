8531e4f387edd2012da4ab8d1d351a55df387d53
Jessica Bohn <bohnjessica15@gmail.com>
2016-08-05 17:27:36 -0500

    Add login controller and view

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,27 +1,27 @@
-#may not need this
-get '/users' do
-	erb :'/users/profile'
-end
+# #may not need this
+# get '/users' do
+# 	erb :'/users/profile'
+# end
 
-get '/users/new' do
-	erb :'/users/new'
-end
+# get '/users/new' do
+# 	erb :'/users/new'
+# end
 
 
-post '/users' do
-end
+# post '/users' do
+# end
 
-get '/users/:id' do
-	erb :'/users/profile'
-end
+# get '/users/:id' do
+# 	erb :'/users/profile'
+# end
 
-get '/users/:id/edit' do
-	erb :'/users/edit'
-end
+# get '/users/:id/edit' do
+# 	erb :'/users/edit'
+# end
 
-put '/users/:id' do
-end
+# put '/users/:id' do
+# end
 
-delete '/users/:id' do
-end
+# delete '/users/:id' do
+# end
 
