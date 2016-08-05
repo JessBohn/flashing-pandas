1e29e233b3c8e5ecdeb21e94aea1349933856bf4
CJ Jessett <email": null>
2016-08-05 16:15:10 -0500

    add guess and round models

---
+++ db/migrate/20160805160404_create_guesses.rb
@@ -0,0 +1,12 @@
+class CreateGuesses < ActiveRecord::Migration
+  def change
+    create_table :guesses do |t|
+      t.string :content
+      t.boolean :correct
+      t.integer :round_id, null: false
+      t.integer :card_id, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
