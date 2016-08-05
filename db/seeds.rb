2c5505ac732ab8cb2c97043e48a65033f6951410
Jessica Bohn <bohnjessica15@gmail.com>
2016-08-05 16:17:27 -0500

    Migrate and model User(s) w/seed

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+User.destroy_all
+
+User.create(username: "gorejess", password_hash: "Lambch0p")
+User.create(username: "tomtom", password_hash: "toms")
+User.create(username: "cj", password_hash: "sandalman")
+User.create(username: "kimpatton", password_hash: "bearlover")
+User.create(username: "mmmmc-gee", password_hash: "itsbrittanybitch")
+User.create(username: "Lamburglar", password_hash: "catastrophe")
+User.create(username: "Catsby", password_hash: "neverloveddaisy")
+User.create(username: "losrojos", password_hash: "cincinnati")
+User.create(username: "DBC", password_hash: "igeekallweek")
+User.create(username: "isthis", password_hash: "katyperry")
